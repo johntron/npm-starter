bc2268da89d0051fc7d6b4cad6f37d5f4ea4986f
John Syrinek <john.syrinek@gmail.com>
2017-10-21 18:02:35 -0500

    feat: inital commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// Notify of updates
+const updateNotifier = require('update-notifier');
+const pkg = require('./package.json');
+
+updateNotifier({ pkg }).notify();
+
+// Track errors
+const Raven = require('raven');
+
+Raven.config('https://792f6beabcc74554a22c207113dd2126@sentry.io/1200860').install();
+
 module.exports = () => true;
